d09935226e584895cbc5e09f3228b2e17930178d
maribelcuales <maribelcuales@gmail.com>
2020-06-23 06:09:21 +0800

    Created Home component,

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <img
+        src="https://github.com/maribelcuales/Sprint-Challenge-Lambda-Eats-starter/blob/master/Assets/Pizza.jpg?raw=true"
+        alt="pizza"
+      />
+    </div>
+
+  );
+};
+
+export default Home; 
