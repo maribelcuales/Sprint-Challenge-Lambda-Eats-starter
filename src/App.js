20ffe814f36ade1835d2eb2624921fae9b070dd9
maribelcuales <maribelcuales@gmail.com>
2020-06-23 04:44:56 +0800

    Added Route paths and Links for home and order form.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Order Pizza</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/pizza">
+          <OrderForm />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
