9c3b670217d7a61e590ba0a1e0f1527635c66df5
maribelcuales <maribelcuales@gmail.com>
2020-06-24 05:58:34 +0800

    Added Reactstrap Container, Row, and Col components.

---
+++ src/components/OrderForm.js
@@ -0,0 +1,20 @@
+import react from "react";
+import { Container, Row, Col } from "reactstrap";
+
+function OrderForm() {
+  return (
+    <Container>
+      <Row>
+        <col>
+          <img 
+            src="https://github.com/maribelcuales/Sprint-Challenge-Lambda-Eats-starter/blob/master/Assets/pizza2.jpg?raw=true"
+            alt="whole pizza"
+          />
+        </col>
+      </Row>
+    </Container>
+
+  );
+};
+
+export default OrderForm;
