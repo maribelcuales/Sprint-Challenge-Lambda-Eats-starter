0aafa7fa09d9af64ba475befc67041bcc4698395
maribelcuales <maribelcuales@gmail.com>
2020-06-25 06:52:56 +0800

    Added h1, img, h3, and name input to OrderForm.

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 import "./styles.css"
 
 function OrderForm() {
   return (
-    <Container className="form-container"> 
-      <Row>
-        <Col>
-          <h1>
-            Build Your Own Pizza
-          </h1>
-          <img 
+    <div className="form-container">
+      <Form>
+        <h1>
+          Build Your Own Pizza
+        </h1>
+        <img 
             src="https://github.com/maribelcuales/Sprint-Challenge-Lambda-Eats-starter/blob/master/Assets/pizza2.jpg?raw=true"
             alt="whole-pizza"
           />
-        </Col>
-      </Row>
-    </Container>
-
+        <div className="order-details">
+          <h3>Build Your Own Pizza</h3>
+          <FormGroup>
+            <Label htmlFor="name">Name</Label>
+            <Input 
+              type="text" 
+              name="name" 
+              id="name" 
+            />
+          </FormGroup>
+        </div>
+      </Form>
+    </div>
   );
 };
 
