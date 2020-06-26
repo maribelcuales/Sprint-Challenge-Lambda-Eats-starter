4bf4083fea200019ee3607f5980a0409de1a9c0a
maribelcuales <maribelcuales@gmail.com>
2020-06-26 16:35:24 +0800

    Created validateChange event handler to validate form inputs.

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -50,6 +50,24 @@ function OrderForm() {
     });
   }, [formState]);
 
+  const validateChange = e => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors, 
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
   const inputChange = e => {
     e.persist();
     const newFormData = {
