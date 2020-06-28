efc9fb17cdc3afddde81d71d673ce628aa472184
maribelcuales <maribelcuales@gmail.com>
2020-06-28 23:38:12 +0800

    Added button to Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "./styles.css";
 
 function Home() {
   return (
-    <div>
-      <img
+    <div className="home-container">
+      <div className="home-image">
+        <img
+        className="home-image"
         src="https://github.com/maribelcuales/Sprint-Challenge-Lambda-Eats-starter/blob/master/Assets/Pizza.jpg?raw=true"
         alt="pizza"
-      />
+        />
+      </div>
+      <div>
+        <button>Pizza ?</button>
+      </div>
     </div>
-
   );
 };
 
