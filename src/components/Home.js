b7fc269a3aa533b500403087f94cc58b827587e3
maribelcuales <maribelcuales@gmail.com>
2020-06-29 22:49:46 +0800

    Added onClick event handler to pizza home button.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./styles.css";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const history = useHistory();
+  console.log("history", history);
+
+  const routeToForm = () => {
+    console.log("Submitting...");
+    setTimeout(() => {
+      history.push("/pizza");
+    }, 2000);
+  };
+
   return (
     <div className="home-container">
       <div className="home-image">
         <img
-        className="home-image"
+        className="home-pizza-image"
         src="https://github.com/maribelcuales/Sprint-Challenge-Lambda-Eats-starter/blob/master/Assets/Pizza.jpg?raw=true"
         alt="pizza"
         />
       </div>
       <div>
-        <button>Pizza ?</button>
+        <button onClick={routeToForm}className="pizza-button" >Pizza ?</button>
       </div>
     </div>
   );
